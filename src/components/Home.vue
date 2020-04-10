063361b62f3ecfae0b7fed6d527289ae9783e116
NOCTLJRNE <baoan1602@gmail.com>
2020-04-10 22:51:27 +0200

    add logout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,9 @@
   <div>
     <h2>Home</h2>
     <User v-slot:user="{ userProp }">
-      <div v-if="userProp">Logged in as {{ userProp.uid }}</div>
+      <div v-if="userProp">
+        <UserProfile v-bind:user="userProp" />
+      </div>
       <Login v-else />
     </User>
   </div>
@@ -11,10 +13,12 @@
 <script>
 import Login from "./Login.vue";
 import User from "./User.vue";
+import UserProfile from "./UserProfile.vue";
 export default {
   components: {
     Login,
     User,
+    UserProfile,
   },
 };
 </script>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Logged In as {{ user.uid }}</p>
+    <button v-on:click="userSignOut">Sign Out</button>
+  </div>
+</template>
+
+<script>
+import { auth } from "../firebase";
+export default {
+  props: {
+    user: {
+      required: false,
+    },
+  },
+  methods: {
+    userSignOut() {
+      auth.signOut();
+    },
+  },
+};
+</script>
+
+<style></style>
