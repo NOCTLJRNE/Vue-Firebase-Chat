0c8a6854f4bfda79a27d53dea23a34fe6e4acb38
NOCTLJRNE <baoan1602@gmail.com>
2020-04-12 00:20:35 +0200

    add realtime chat & messages collection

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,15 +1,71 @@
 <template>
   <main class="section">
-    <h2>Welcome to {{ chatRoomID }}</h2>
+    <h2>Welcome to Chat Room {{ chatRoomID }}</h2>
     <router-link to="/">Back</router-link>
+    <User v-slot:user="{ userProp }">
+      <ul>
+        <li v-for="message of messages" :key="message.id">
+          {{ message.text }}
+        </li>
+      </ul>
+      <input
+        type="text"
+        v-model="newTextMessage"
+        placeholder="Say something :D"
+        class="input"
+      />
+      <button
+        class="button is-success"
+        v-bind:disabled="!newTextMessage || loading"
+        v-on:click="sendMessage(userProp.uid)"
+      >
+        Send
+      </button>
+    </User>
   </main>
 </template>
 
 <script>
+import User from "./User.vue";
+import { db } from "../firebase";
 export default {
+  components: {
+    User,
+  },
+  data() {
+    return {
+      messages: [],
+      newTextMessage: "",
+      loading: false,
+    };
+  },
+  firestore() {
+    return {
+      messages: this.messagesCollection.orderBy("createdAt").limitToLast(10),
+    };
+  },
+  methods: {
+    async sendMessage(uid) {
+      this.loading = true;
+      const { id: messageID } = this.messagesCollection.doc();
+      await this.messagesCollection.doc(messageID).set({
+        createdAt: Date.now(),
+        sender: uid,
+        text: this.newTextMessage,
+      });
+      this.loading = false;
+      this.newTextMessage = "";
+    },
+  },
   computed: {
     chatRoomID() {
       return this.$route.params.id;
+    },
+    messagesCollection() {
+      return db
+        .collection("chats")
+        .doc(`${this.chatRoomID}`)
+        .collection("messages");
     },
   },
 };
