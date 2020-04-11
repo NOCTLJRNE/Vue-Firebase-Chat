029010427454b3cbb826b5a79e72a5c2ea29fb36
NOCTLJRNE <baoan1602@gmail.com>
2020-04-11 22:24:42 +0200

    add (missing files) dynamic routing chat room

---
+++ src/components/ChatRoom.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="section">
+    <h2>Welcome to {{ chatRoomID }}</h2>
+    <router-link to="/">Back</router-link>
+  </main>
+</template>
+
+<script>
+export default {
+  computed: {
+    chatRoomID() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
+
+<style></style>
