b58ae4332c73c1c2ef5346f2eaa4b5934bd3ee12
NOCTLJRNE <baoan1602@gmail.com>
2020-04-11 22:15:02 +0200

    add dynamic routing chat room

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -2,7 +2,11 @@
   <div>
     <button v-on:click="createChatRoom">Create Chat Room</button>
     <ul>
-      <li v-for="chat in chats" :key="chat.id">{{ chat.id }}</li>
+      <li v-for="chat in chats" :key="chat.id">
+        <router-link v-bind:to="{ name: 'chat', params: { id: chat.id } }">{{
+          chat.id
+        }}</router-link>
+      </li>
     </ul>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import { firestorePlugin } from "vuefire";
 import VueRouter from "vue-router";
 import Home from "./components/Home.vue";
+import ChatRoom from "./components/ChatRoom.vue";
 import VueCompositionApi from "@vue/composition-api";
 
 Vue.config.productionTip = false;
@@ -10,7 +11,10 @@ Vue.use(firestorePlugin);
 Vue.use(VueRouter);
 Vue.use(VueCompositionApi);
 
-const routes = [{ path: "/", component: Home }];
+const routes = [
+  { path: "/", component: Home },
+  { path: "chats/:id", name: "chat", component: ChatRoom },
+];
 const router = new VueRouter({
   routes,
 });
